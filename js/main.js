5b8f21ceea61567faa1ad17e9b54ad038fe0ea79
Andrew Chung <andrew.chung159@gmail.com>
2015-07-22 12:35:59 -0400

    update about section with skills boxes

--- js/main.js
+++ js/main.js
@@ -1,11 +1,19 @@
 var menuToggle = function() {
 	$('.menu-icon').click(function() {
-		$('#mobile-menu-group').slideToggle();
+		$('#mobile-menu-group').slideToggle(700);
 	});
 };
 
+var skillsToggle = function() {
+	$('.skills-heading').click(function() {
+		$(this).next('.panel-body').slideToggle(700);
+	});
+};
+
+
 $(document).ready(function() {
 	menuToggle();
+	skillsToggle();
 });
 
 
