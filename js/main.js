66b6667ddb4daa3f23dfc31b7547e9d5195aee8a
Andrew Chung <andrew.chung159@gmail.com>
2015-07-22 22:12:35 -0400

    add smooth sliding on section link click; update personal-header img

--- js/main.js
+++ js/main.js
@@ -1,3 +1,17 @@
+var $root = $('html, body');
+
+var slideToAnchor = function() {
+	$('a').click(function() {
+	    var href = $(this).attr('href');
+	    $root.animate({
+	        scrollTop: $(href).offset().top
+	    }, 700, function () {
+	        window.location.hash = href;
+	    });
+	    return false;
+	});
+};
+
 var menuToggle = function() {
 	$('.menu-icon').click(function() {
 		$('#mobile-menu-group').slideToggle(700);
@@ -12,8 +26,7 @@ var skillsToggle = function() {
 
 
 $(document).ready(function() {
+	slideToAnchor();
 	menuToggle();
 	skillsToggle();
 });
-
-
